51fb673d2ee5a214e54175ded44b65dc7bc1f13e
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-04-11 12:42:14 -0500

    Added default colors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,39 @@
 import { ReactElement } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Home from './routes/home/home.component';
 import Sidebar from './routes/sidebar/sidebar.component';
 
-import './App.css';
-
-function App() : ReactElement {
-
-
+function Layout(): ReactElement {
   return (
     <Container fluid>
       <Row>
-        <Col xs={3} lg={2} className="px-3  text-white bg-dark sidebar" >
+        <Col xs={3} lg={2} className="px-3 sidebar">
           <Sidebar />
         </Col>
-        <Col xs={9} lg={10} className="px-5 py-5">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<p>Projects</p>} />
-            <Route path="/work" element={<p>Work</p>} />
-            
-          </Routes>
+        <Col xs={9} lg={10} className="px-5 py-5 main">
+          <Outlet /> {/* Renders child routes */}
         </Col>
       </Row>
     </Container>
-  )
+  );
 }
 
-export default App
+function App(): ReactElement {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} /> {/* Default route */}
+        <Route path="projects" element={<p>Projects</p>} />
+        <Route path="work" element={<p>Work</p>} />
+        <Route path="skills" element={<p>Skills</p>} />
+        <Route path="education" element={<p>Education</p>} />
+        <Route path="contact" element={<p>Contact</p>} />
+        <Route path="about" element={<p>About</p>} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
