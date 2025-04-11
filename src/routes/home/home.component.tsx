96517bdcad55b1edbf9ab34198d23e113a973fe2
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-04-11 15:27:22 -0500

    Implemented Home content.

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,15 +1,32 @@
-import { ReactElement } from 'react'
-
-
+import { Fragment, ReactElement } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import HomeSelection from './home-selection/home_selection.component'
+import './home.style.css'
 
 function Home() : ReactElement {
 
     return  (
-        <div className="home">
-            <h1>Home</h1>
-            <p>Welcome to the home page!</p>
-            <p>This is a simple React application.</p>  
-        </div>
+        <Fragment>
+            <Container>
+                <Row>
+                    <Col lg={5}>
+                        <Row  className='home_selection'>
+                            <HomeSelection/>
+                        </Row>
+                        <Row   className='home_description'>
+                            <p>Built by faith. Driven by curiosity. Creating with code.</p>
+                            <p>23 Work willingly at whatever you do, as though you were working for the Lord rather than for people.
+                            Colossians 3:23</p>
+                        </Row>
+                    </Col>
+                    <Col lg={7} >
+                        <Row className='home_image_container'>
+            
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </Fragment>
     )
 }
 
