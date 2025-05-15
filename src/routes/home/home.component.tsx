1f081c65e13067080fd395e9d05b6c9729ad58ef
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-15 18:38:18 -0500

    Implemented a Project Highlights section.

--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,10 +1,9 @@
-import { Fragment, ReactElement } from 'react';
-import { Stack } from 'react-bootstrap';
-import ProfileSection from './profile_section/profile_section.component';
-import './home.style.css';
-import GitHubCalendar from 'react-github-calendar';
-
-
+import { Fragment, ReactElement } from "react";
+import { Stack } from "react-bootstrap";
+import GitHubCalendar from "react-github-calendar";
+import "./home.style.css";
+import ProfileSection from "./profile_section/profile_section.component";
+import ProjectHighlights from "./project_highlight/project_highlight.component";
 
 /**
  * The `Home` component represents the main home page of the application.
@@ -17,30 +16,31 @@ import GitHubCalendar from 'react-github-calendar';
  * - `handleSelectionChange(selectedIndex: number): void`:
  *   Updates the description and GIF based on the provided index.
  */
-function Home() : ReactElement {
+function Home(): ReactElement {
     const descriptions: string[] = [
-        'Built by faith. Driven by curiosity. Creating with code.',
-        'I build reliable, efficient systems from front to back. Every solution is tailored, no shortcuts, just clean, purposeful code.',
-        'Clean, efficient, and reliable. If it’s on your desktop, it’s meant to help you get things done without the noise.',
-        'From mechanics to storytelling, every detail is designed to deliver a challenging and rewarding experience.'
-    ]
+        "Built by faith. Driven by curiosity. Creating with code.",
+        "I build reliable, efficient systems from front to back. Every solution is tailored, no shortcuts, just clean, purposeful code.",
+        "Clean, efficient, and reliable. If it’s on your desktop, it’s meant to help you get things done without the noise.",
+        "From mechanics to storytelling, every detail is designed to deliver a challenging and rewarding experience.",
+    ];
 
     const gifs_path: string[] = [
-        'src\\assets\\goku vegeta.gif',
-        'src\\assets\\banana.gif',
-        'src\\assets\\killua.gif',
-        'src\\assets\\obito.gif'
-    ]
+        "src\\assets\\goku vegeta.gif",
+        "src\\assets\\banana.gif",
+        "src\\assets\\killua.gif",
+        "src\\assets\\obito.gif",
+    ];
 
-
-    return  (
+    return (
         <Fragment>
-            <Stack direction='horizontal' gap={1} className='home_container'>
-                <ProfileSection/>
-                <Stack className='right' gap={2}>
-                    <div className='ascii_content'>asd</div>
-                    <div className='projects_favorites'>asd</div>
-                    <div className='github_calendar'>
+            <Stack direction="horizontal" gap={1} className="home_container">
+                <ProfileSection />
+                <Stack className="right" gap={2}>
+                    <div className="project_highlights">
+                        <ProjectHighlights />
+                    </div>
+                    <div className="projects_favorites">asd</div>
+                    <div className="github_calendar">
                         <GitHubCalendar
                             username="alcoranpaul"
                             year="last"
@@ -52,7 +52,7 @@ function Home() : ReactElement {
                 </Stack>
             </Stack>
         </Fragment>
-    )
+    );
 }
 
-export default Home
+export default Home;
---
+++ src/routes/home/project_highlight/project_highlight.component.tsx
@@ -0,0 +1,32 @@
+import { ReactElement } from "react";
+import { Carousel } from "react-bootstrap";
+
+export default function ProjectHighlights(): ReactElement {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img src="src\\assets\\obito.gif" alt="First slide" />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img src="src\\assets\\banana.gif" alt="Second slide" />
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img src="src\\assets\\goku vegeta.gif" alt="Third slide" />
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
