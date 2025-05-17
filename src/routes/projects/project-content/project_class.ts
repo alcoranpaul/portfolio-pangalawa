ea20849a3e3870d71a860c2d51522ccdd6fea768
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-17 02:30:43 -0500

    Refactor to include new attributes.

--- src/routes/projects/project-content/project_class.ts
+++ src/routes/projects/project-content/project_class.ts
@@ -1,17 +1,38 @@
 import master_list from "../../../json/projects/master_list_projects.json";
 export class ProjectData {
     name: string;
-    description: string;
     image: string;
+    description: string;
+    short_description: string;
+    markdown: string;
+    tech_stack: string;
     startDate: Date;
+    source_link: string;
+    live_link: string;
     endDate?: Date;
 
-    constructor(name: string, description: string, startDate: Date, image: string, endDate?: Date) {
+    constructor(
+        name: string,
+        image: string,
+        description: string,
+        short_description: string,
+        markdown: string,
+        tech_stack: string,
+        startDate: Date,
+        source_link: string,
+        live_link: string,
+        endDate?: Date
+    ) {
         this.name = name;
-        this.description = description;
-        this.startDate = startDate;
         this.image = image;
+        this.description = description;
+        this.short_description = short_description;
+        this.markdown = markdown;
+        this.tech_stack = tech_stack;
+        this.startDate = startDate;
         this.endDate = endDate;
+        this.source_link = source_link;
+        this.live_link = live_link;
     }
 
     isImageALink(): boolean {
@@ -34,10 +55,15 @@ export class ProjectData {
 // Ensure TypeScript knows the shape of each object in the JSON
 interface ProjectJSON {
     name: string;
-    description: string;
     image: string;
-    startDate: string;
-    endDate?: string;
+    description: string;
+    short_description: string;
+    markdown: string;
+    tech_stack: string;
+    startDate: Date;
+    source_link: string;
+    live_link: string;
+    endDate?: Date;
 }
 
 // Type assertion to tell TS this is an array of the right shape
@@ -48,9 +74,19 @@ export const projectDataList: ProjectData[] = rawData.map(
     (data) =>
         new ProjectData(
             data.name,
-            data.description,
-            new Date(data.startDate),
             data.image,
+            data.description,
+            data.short_description,
+            data.markdown,
+            data.tech_stack,
+            new Date(data.startDate),
+            data.source_link,
+            data.live_link,
             data.endDate ? new Date(data.endDate) : undefined
         )
 );
+
+export function getProjectData(name: string | undefined): ProjectData | undefined {
+    if (name === undefined) return undefined;
+    return projectDataList.find((item) => item.name === name);
+}
