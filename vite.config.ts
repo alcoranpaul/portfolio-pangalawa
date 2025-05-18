56928a3344a535237e22dd0eab53b467dea62053
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-17 21:19:59 -0500

    Added deployment workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-        '/api': {
-            target: 'https://any-anime-api.vercel.app',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
+    plugins: [react()],
+    base: "/portfolio-pangalawa/",
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://any-anime-api.vercel.app",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
         },
     },
-},
-})
+});
