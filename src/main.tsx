4634cc5446d7672df14b0b330e279b85b56ce61e
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-05-17 22:51:38 -0500

    Convert to using hashrouter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename="/portfolio-pangalawa">
+        <HashRouter basename="/portfolio-pangalawa">
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 );
